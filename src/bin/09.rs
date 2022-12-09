9b5f0f32b61bd0c6c94edbe5a4a83d6bb49c2c4a
Steven Lotito <steven.lotito@gmail.com>
2022-12-09 12:31:30 -0800

    refactor day 9, part 2 not yet working, will likely need to visualize

--- src/bin/09.rs
+++ src/bin/09.rs
@@ -1,23 +1,34 @@
 use std::collections::BTreeSet;
 
+#[derive(Debug, Clone)]
+struct Knot {
+    pos: (i16, i16),
+    prev: (i16, i16),
+    visited: BTreeSet<(i16, i16)>,
+}
+
+impl Knot {
+    pub fn new() -> Self {
+        let mut n = Self {
+            pos: (0, 0),
+            prev: (0, 0),
+            visited: BTreeSet::new(),
+        };
+        n.visited.insert((0, 0));
+        n
+    }
+}
+
 #[derive(Debug)]
 struct Board {
-    visited: BTreeSet<(i16, i16)>,
-    h_pos: (i16, i16),
-    trailing: (i16, i16),
-    t_pos: (i16, i16),
+    knots: Vec<Knot>,
 }
 
 impl Board {
-    pub fn new() -> Self {
-        let mut b = Board {
-            visited: BTreeSet::new(),
-            h_pos: (0, 0),
-            trailing: (0, 0),
-            t_pos: (0, 0),
-        };
-        b.visited.insert(b.t_pos.clone());
-        b
+    pub fn new(num_knots: usize) -> Self {
+        Board {
+            knots: vec![Knot::new(); num_knots],
+        }
     }
 
     pub fn process_move_cmd(&mut self, command: &str) {
@@ -33,29 +44,27 @@ impl Board {
         let amount = iter.next().unwrap().parse::<i16>().unwrap();
         for _ in 0..amount {
             self.move_head(dir);
-            self.move_tail();
+            for n in 1..self.knots.len() {
+                self.move_n(n);
+            }
         }
     }
 
     pub fn move_head(&mut self, dir: (i16, i16)) {
-        self.trailing = self.h_pos;
-        self.h_pos.0 += dir.0;
-        self.h_pos.1 += dir.1;
+        self.knots[0].prev = self.knots[0].pos;
+        let curr = self.knots[0].pos;
+        let next = (curr.0 + dir.0, curr.1 + dir.1);
+        self.knots[0].pos = next;
+        self.knots[0].visited.insert(next);
     }
 
-    pub fn move_tail(&mut self) {
-        // if t_pos is within 1 space of h_pos, then just return
-        let distance = Self::distance(self.h_pos, self.t_pos);
-        // dbg!(&distance);
+    /// Move the nth knot to maintain the correct distance to the n-1 knot
+    pub fn move_n(&mut self, n: usize) {
+        let distance = Self::distance(self.knots[n - 1].pos, self.knots[n].pos);
         if distance >= 2.0 {
-            // println!(
-            //     "moving tail {:?} to {:?}, a space toward head? {:?}, distance was {:?}",
-            //     self.t_pos, self.trailing, self.h_pos, distance
-            // );
-            self.t_pos = self.trailing;
-            self.visited.insert(self.t_pos);
-        } else {
-            // println!("*** skipping tail move, within 1 space ***");
+            let trailing = self.knots[n - 1].prev;
+            self.knots[n].pos = trailing;
+            self.knots[n].visited.insert(trailing);
         }
     }
 
@@ -71,15 +80,19 @@ impl Board {
 }
 
 pub fn part_one(input: &str) -> Option<usize> {
-    let mut b = Board::new();
+    let mut b = Board::new(2);
     for cmd in input.lines() {
         b.process_move_cmd(cmd);
     }
-    Some(b.visited.len())
+    Some(b.knots[1].visited.len())
 }
 
 pub fn part_two(input: &str) -> Option<usize> {
-    None
+    let mut b = Board::new(10);
+    for cmd in input.lines() {
+        b.process_move_cmd(cmd);
+    }
+    Some(b.knots[9].visited.len())
 }
 
 fn main() {
@@ -101,6 +114,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 9);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(1));
     }
 }
