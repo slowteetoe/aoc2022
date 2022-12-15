9e0f525fd95492cd3c9bc1ac37e476c9459f1608
Steven Lotito <steven.lotito@gmail.com>
2022-12-14 21:34:26 -0800

    need to work on comparisons, but data structure and parsing work

--- src/bin/13.rs
+++ src/bin/13.rs
@@ -1,14 +1,59 @@
 use core::fmt;
+use std::cell::RefCell;
 
 use itertools::Itertools;
 
-// why not, let's get complicated
+// why not, let's get complicated and make a list structure!
 type LvalChildren = Vec<Box<Lval>>;
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum Lval {
+    Tombstone,
     Num(u8),
     List(LvalChildren),
+}
+
+#[derive(Debug)]
+pub struct Packet(Vec<Box<Lval>>);
+
+#[derive(Debug)]
+pub struct PacketPair {
+    p1: Packet,
+    p2: Packet,
+}
+
+impl PacketPair {
+    pub fn in_order(&self) -> bool {
+        println!("comparing {:?} and {:?}", self.p1.0, self.p2.0);
+        for (left, right) in self.p1.0.iter().zip(self.p2.0.iter()) {
+            println!("comparing {:?} and {:?}", left, right);
+            match (&**left, &**right) {
+                (Lval::Num(l), Lval::Num(r)) => {
+                    if l > r {
+                        println!("left num is larger than right num, not in order");
+                        return false;
+                    }
+                }
+                (Lval::List(l), Lval::List(r)) => {
+                    if l.len() > r.len() {
+                        println!("right list is shorter, so can't be in order");
+                        return false;
+                    }
+                    // otherwise we have to check each element
+                    // but this will need to be recursive I think since lists can be lists of lists
+                }
+                (Lval::Num(l), Lval::List(r)) => {
+                    // make left a list and call compare on the two
+                }
+                (Lval::List(l), Lval::Num(r)) => {
+                    // make right a list and call compare on the two
+                }
+                _ => unreachable!(""),
+            }
+        }
+        println!("*** packets in order ***");
+        true
+    }
 }
 
 impl fmt::Display for Lval {
@@ -16,50 +61,69 @@ impl fmt::Display for Lval {
         match self {
             Lval::Num(n) => write!(f, "{}", n),
             Lval::List(children) => write!(f, "({})", rec_print(&children)),
+            Lval::Tombstone => write!(f, "🪦"),
         }
     }
 }
 
-// going to have a recursive datastructure so we'll need a Box
-#[derive(Debug)]
-pub struct Packet(Vec<Box<Lval>>);
-
 impl Packet {
-    // oh i bet theres a fun regex for this...
-    // pub fn parse2(p: &str) -> Packet {
-    //     println!("parsing {:?}", &p);
-
-    //     let mut stack = vec![];
-
-    //     for ch in p.chars() {
-    //         match ch {
-    //             '[' => stack.push(-1),
-    //             ']' => {
-    //                 let thisvec = vec![];
-    //                 while let v = match stack.pop() {
-    //                 Some(val) => if val == -1 { stack.push(vec![]) } else { stack.push},
-    //                 None => unreachable!("well crap.")
-    //                 };
-    //             },
-    //             ',' => todo!("ignore"),
-    //             _ => todo!("convert to u8")
-    //         }
-    //     }
-    //     Packet(vec![])
-    // }
-
     pub fn parse(p: &str) -> Packet {
-        Packet(vec![
-            Self::lval_num(6),
-            Self::listOf(vec![Lval::Num(7), Lval::Num(8)]),
-            Self::listOf(vec![
-                *Self::listOf(vec![Lval::Num(9)]),
-                *Self::listOf(vec![]),
-            ]),
-        ])
+        let mut stack = vec![];
+        let mut thisvec = RefCell::new(Vec::new());
+
+        for ch in p.chars() {
+            // println!("looking at {}, stack currently {:?}", ch, &stack);
+            match ch {
+                '[' => stack.push(Box::new(Lval::Tombstone)),
+                '0'..='9' => {
+                    stack.push(Box::new(Lval::Num(ch.to_string().parse::<u8>().unwrap())));
+                }
+                ']' => {
+                    // start unwinding until we hit a tombstone, reverse the vec and put it back on the stack
+                    // println!("poppin like it's hot...");
+                    loop {
+                        let val = stack.pop();
+                        if val.is_none() {
+                            // hit the end of the stack, nothing left
+                            unreachable!("shouldn't bottom out");
+                        }
+                        let item = val.unwrap();
+                        match *item {
+                            Lval::List(contents) => {
+                                thisvec.borrow_mut().push(Box::new(Lval::List(contents)));
+                            }
+                            Lval::Num(_) => {
+                                thisvec.borrow_mut().push(item);
+                            }
+                            Lval::Tombstone => {
+                                // see what we have, push back onto the stack as a list
+                                if stack.is_empty() {
+                                    // we're at the last tombstone, so it's complete
+                                    let mut result = thisvec.get_mut().to_vec();
+                                    result.reverse();
+                                    return Packet(result);
+                                } else {
+                                    // println!("done popping stack, pushing the list we just built up back on");
+                                    let mut tmp = thisvec.get_mut().to_vec();
+                                    tmp.reverse();
+                                    stack.push(Box::new(Lval::List(tmp)));
+                                }
+                                thisvec = RefCell::new(Vec::new());
+                                break;
+                            }
+                        }
+                    }
+                }
+                ',' => {
+                    continue;
+                }
+                _ => unreachable!("invalid chars in input"),
+            }
+        }
+        unreachable!("unbalanced input if we see this");
     }
 
-    pub fn listOf(stuff: Vec<Lval>) -> Box<Lval> {
+    pub fn list_of(stuff: Vec<Lval>) -> Box<Lval> {
         let mut children = vec![];
         for s in stuff {
             children.push(Box::new(s))
@@ -83,6 +147,9 @@ impl fmt::Display for Packet {
                 Lval::List(children) => {
                     result.push(format!("({})", rec_print(&children)));
                 }
+                Lval::Tombstone => {
+                    result.push(String::from("🪦"));
+                }
             }
         }
         write!(f, "{}", result.join(","))
@@ -97,21 +164,9 @@ fn rec_print(children: &[Box<Lval>]) -> String {
     resp.join(",")
 }
 
-#[derive(Debug)]
-pub struct PacketPair {
-    p1: Packet,
-    p2: Packet,
-}
-
-impl PacketPair {
-    pub fn in_order(&self) -> bool {
-        true
-    }
-}
-
 impl fmt::Display for PacketPair {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "p1:{} p2:{}", &self.p1, &self.p2)
+        write!(f, "p1: [{}], p2: [{}]", &self.p1, &self.p2)
     }
 }
 
