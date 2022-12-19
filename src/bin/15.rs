d7a772efa15b635788cdc6b696d51271cd24db08
Steven Lotito <steven.lotito@gmail.com>
2022-12-19 12:12:32 -0800

    fast, but not correct!

--- src/bin/15.rs
+++ src/bin/15.rs
@@ -1,8 +1,12 @@
-use std::collections::{BTreeMap, BTreeSet};
+use std::{
+    cmp,
+    collections::{BTreeMap, BTreeSet},
+};
 
+use advent_of_code::helpers::{intersection, manhattan};
 use regex::Regex;
 
-pub fn read_sensors(input: &str) -> (BTreeMap<(i32, i32), usize>, BTreeSet<(i32, i32)>) {
+pub fn read_sensors(input: &str) -> (BTreeMap<(i64, i64), usize>, BTreeSet<(i64, i64)>) {
     // for part 1 at least, we don't really need to keep track of the beacon locations
     let re =
         Regex::new(r"Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)")
@@ -12,12 +16,12 @@ pub fn read_sensors(input: &str) -> (BTreeMap<(i32, i32), usize>, BTreeSet<(i32,
     for line in input.lines() {
         let cap = re.captures(line).unwrap();
         let (x1, y1) = (
-            cap[1].parse::<i32>().unwrap(),
-            cap[2].parse::<i32>().unwrap(),
+            cap[1].parse::<i64>().unwrap(),
+            cap[2].parse::<i64>().unwrap(),
         );
         let (x2, y2) = (
-            cap[3].parse::<i32>().unwrap(),
-            cap[4].parse::<i32>().unwrap(),
+            cap[3].parse::<i64>().unwrap(),
+            cap[4].parse::<i64>().unwrap(),
         );
         sensor_map.insert((x1, y1), manhattan((x1, y1), (x2, y2)));
         beacons.insert((x2, y2));
@@ -27,70 +31,122 @@ pub fn read_sensors(input: &str) -> (BTreeMap<(i32, i32), usize>, BTreeSet<(i32,
     (sensor_map, beacons)
 }
 
-pub fn manhattan(p1: (i32, i32), p2: (i32, i32)) -> usize {
-    (p1.0.abs_diff(p2.0) + p1.1.abs_diff(p2.1)) as usize
-}
-
 pub fn part_one(input: &str) -> Option<usize> {
     let sensors = read_sensors(input);
-    let mut excluded = BTreeSet::new();
 
-    // let target = 2_000_000i32;
-    let target = 10i32;
+    let target = 2_000_000i64;
+    // let target = 9i64;
+
+    let mut ranges = vec![];
 
     for (sensor, dist) in sensors.0 {
         // now we're going to iterate over the sensor map and calculate the points where a beacon cannot exist
         // if that point intersects with y=200_000 then we'll add it to a set
-
-        if sensor.1.abs_diff(target) > dist.try_into().unwrap() {
+        println!(
+            "sensor({:?},{:?}) with scanning distance of {:?}",
+            sensor.0, sensor.1, dist
+        );
+        if sensor.1.abs_diff(target) <= dist.try_into().unwrap() {
             // let's try dropping any work that can't hit our target
-            println!("skipping ({:?},{:?})", sensor.0, sensor.1);
+            println!("\tintersects target line");
+            let d = dist as i64;
+            let up = (sensor.0, sensor.1 + d);
+            let right = (sensor.0 + d, sensor.1);
+            let down = (sensor.0, sensor.1 - d);
+            let left = (sensor.0 - d, sensor.1);
+
+            let target_line = ((-1000, target), (1000, target));
+
+            // now figure out which lines intersect target line, it should always be the \/ or /\ lines
+            let p1 = intersection((left, down), target_line);
+            if p1.is_some() {
+                println!("intersected left down, looking for other point");
+                let p2 = intersection((right, down), target_line);
+                ranges.push((p1.unwrap().0, p2.unwrap().0)); // these will all be on the y (target) axis
+            } else {
+                let p1 = intersection((left, up), target_line);
+                if p1.is_none() {
+                    panic!("I thought this intersected, but apparently not!");
+                }
+                let p2 = intersection((right, down), target_line);
+                ranges.push((p1.unwrap().0, p2.unwrap().0));
+            }
+        } else {
+            println!("\tcan't reach the target line");
             continue;
         }
 
-        calculate_exclusion_zone(sensor, dist, target)
-            .iter()
-            .filter(|(_x, y)| *y == target)
-            .for_each(|pt| {
-                excluded.insert(pt.clone());
-            });
+        // calculate_exclusion_zone(sensor, dist, target)
+        //     .iter()
+        //     .filter(|(_x, y)| *y == target)
+        //     .for_each(|pt| {
+        //         excluded.insert(pt.clone());
+        //     });
     }
-    for pt in sensors.1 {
-        if pt.1 == target {
-            println!("Beacon at {:?}", &pt);
-            excluded.remove(&pt.clone());
-        }
+    // for pt in sensors.1 {
+    //     if pt.1 == target {
+    //         println!("Beacon at {:?}", &pt);
+    //         excluded.remove(&pt.clone());
+    //     }
+    // }
+
+    // FIXME now we need to process the ranges, merging together
+    ranges.sort();
+    println!("sensor ranges (that we need to grow): {:?}", ranges);
+    let final_ranges = collapse(ranges);
+    if final_ranges.len() == 1 {
+        return Some(final_ranges[0].1.abs_diff(final_ranges[0].0) as usize);
     }
-    Some(excluded.len())
+    None
 }
 
-pub fn calculate_exclusion_zone(
-    origin: (i32, i32),
-    dist: usize,
-    target_y: i32,
-) -> BTreeSet<(i32, i32)> {
-    // I'm tired, lets just be inefficient - OOPS this only works for the examples, guess I have to come up with an actual algorithm
-    let mut candidates = BTreeSet::new();
-    candidates.insert((origin.0, origin.1));
-
-    for x in (origin.0 - dist as i32)..=(origin.0 + dist as i32) {
-        for y in (origin.1 - dist as i32)..=(origin.1 + dist as i32) {
-            if y == target_y {
-                candidates.insert((x, y));
-            }
+pub fn collapse(ranges: Vec<(i64, i64)>) -> Vec<(i64, i64)> {
+    println!("**** collapsing: {:?}", ranges);
+    if ranges.len() == 1 {
+        println!("done, only one contiguous range left");
+        return ranges;
+    }
+    if ranges.len() == 2 {
+        println!("two ranges left, let's try to merge them");
+        if ranges[0].1 > ranges[1].0 {
+            println!("merging final pair");
+            return vec![(ranges[0].0, cmp::max(ranges[0].1, ranges[1].1))];
         }
+
+        return ranges;
     }
 
-    // println!(
-    //     "candidates within distance {} of {:?} are: {:?}",
-    //     dist, &origin, candidates,
-    // );
-    candidates
-        .into_iter()
-        .filter(|pt| manhattan(origin, *pt) <= dist)
-        .collect()
+    let mut collapsed = vec![];
+
+    // let's try to collapse every 2 ...
+    for n in 1..=ranges.len() / 2 {
+        let left = ranges[2 * n - 1 - 1];
+        let right = ranges[2 * n - 1];
+        println!("looking at {:?} and {:?}", left, right);
+        if left.1 > right.0 {
+            // merge
+            let end = cmp::max(left.1, right.1);
+            println!("merging ranges to {:?}", (left.0, end));
+            collapsed.push((left.0, end));
+        } else {
+            println!("couldn't merge");
+            collapsed.push(left);
+            collapsed.push(right);
+        }
+    }
+    if ranges.len() % 2 == 1 {
+        println!("odd");
+        // if there was an odd number, we didn't try to collapse the last val so add it back
+        collapsed.push(ranges[ranges.len() - 1]);
+    }
+
+    return collapse(collapsed);
 }
 
+pub fn calculate_exclusion_zone(origin: (i64, i64), dist: usize, target_y: i64) {}
+
+// https://www.reddit.com/r/adventofcode/comments/zmjzu7/2022_day_15_part_2_no_search_formula/
+// -or- https://github.com/emlun/adventofcode-2022/blob/9dadc35bb4ebbb8352f9525f0eeaf2ea2babd766/src/days/day15.rs#L57-L91
 pub fn part_two(input: &str) -> Option<u32> {
     None
 }
--- src/helpers.rs
+++ src/helpers.rs
@@ -2,3 +2,31 @@
  * Use this file if you want to extract helpers from your solutions.
  * Example import from this file: `use advent_of_code::helpers::example_fn;`.
  */
+
+pub fn manhattan(p1: (i64, i64), p2: (i64, i64)) -> usize {
+    (p1.0.abs_diff(p2.0) + p1.1.abs_diff(p2.1)) as usize
+}
+
+pub fn intersection(
+    line1: ((i64, i64), (i64, i64)),
+    line2: ((i64, i64), (i64, i64)),
+) -> Option<(i64, i64)> {
+    let (start1, end1) = line1;
+    let (start2, end2) = line2;
+
+    let a1 = end1.1 - start1.1;
+    let b1 = start1.0 - end1.0;
+    let c1 = a1 * start1.0 + b1 * start1.1;
+
+    let a2 = end2.1 - start2.1;
+    let b2 = start2.0 - end2.0;
+    let c2 = a2 * start2.0 + b2 * start2.1;
+
+    let delta = a1 * b2 - a2 * b1;
+
+    if delta == 0 {
+        return None;
+    }
+
+    Some(((b2 * c1 - b1 * c2) / delta, (a1 * c2 - a2 * c1) / delta))
+}
