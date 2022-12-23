6baebd91d50881eea8a9cf7d74beb6c8b9fd92c9
Steven Lotito <steven.lotito@gmail.com>
2022-12-22 17:14:00 -0800

    day 20, still not working

--- src/bin/20.rs
+++ src/bin/20.rs
@@ -1,11 +1,13 @@
 use std::collections::VecDeque;
+use tap::tap::Tap;
 
 pub fn parse(input: &str) -> Vec<i64> {
     input.lines().map(|n| n.parse::<i64>().unwrap()).collect()
+    // [1, 2, -3, 3, -2, 0, 4].to_vec()
 }
 
 // not mine, credit to https://gist.github.com/samueltardieu/3c80720af87d78e5b034afdb57d147b0
-// but it doesn't come up with the right answer =/
+// but it doesn't come up with the right answer for the input set =/
 fn main() {
     let input = &advent_of_code::read_file("inputs", 20);
     advent_of_code::solve!(1, part_one, input);
@@ -21,13 +23,18 @@ pub fn part_one(input: &str) -> Option<i64> {
         .collect::<VecDeque<_>>();
 
     for i in 0..values.len() {
-        // find the position of the item in the current list
+        // find the position of the item in the current list by using the original index
         let idx = values.iter().enumerate().position(|(j, _)| i == j).unwrap();
+
         // rotate so that it's at the beginning of the list
         values.rotate_left(idx);
+        // pop the (orig pos, value) list off
         let (j, val) = values.pop_front().unwrap();
+        // figure out number of places it should be rotated
         let d = val.rem_euclid(values.len() as i64) as usize;
+        // rotate to that position
         values.rotate_left(d);
+        // put the (orig pos, value) tuple back into the list
         values.push_front((j, val));
     }
     // now that it's all rotated, find where our 0 went
