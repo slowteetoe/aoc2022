a9cf19e965121e0ef74dbd47ed59b676d5770b2a
Steven Lotito <steven.lotito@gmail.com>
2022-12-23 17:52:44 -0800

    day 21 part 2 not working

--- src/bin/21.rs
+++ src/bin/21.rs
@@ -33,6 +33,48 @@ pub fn part_one(input: &str) -> Option<usize> {
             let b_monkey = resolved.get(b);
             match (a_monkey, b_monkey) {
                 (Some(a), Some(b)) => {
+                    // println!("Resolved {}", v);
+                    let result = match op {
+                        "+" => a + b,
+                        "-" => a - b,
+                        "*" => a * b,
+                        "/" => a / b,
+                        _ => unreachable!(),
+                    };
+                    resolved.insert(k.to_string(), result);
+                    remove.push(k.clone());
+                }
+                _ => (),
+            }
+        }
+        for key in remove {
+            pending.remove(&key);
+        }
+        // println!(
+        //     "After {} round(s): pending {:?} resolved {:?}",
+        //     round,
+        //     pending.len(),
+        //     resolved.len()
+        // );
+        if resolved.contains_key("root") {
+            return Some(resolved.get("root").unwrap().clone());
+        }
+    }
+    None
+}
+
+pub fn part_two(input: &str) -> Option<usize> {
+    let (mut resolved, mut pending) = parse(input);
+    resolved.remove("humn");
+    for _ in 0..256 {
+        let mut remove = vec![];
+        for (k, v) in pending.iter_mut() {
+            // split value on spaces, we'll end up with monkey op monkey
+            let (a, op, b) = v.splitn(3, " ").next_tuple().unwrap();
+            let a_monkey = resolved.get(a);
+            let b_monkey = resolved.get(b);
+            match (a_monkey, b_monkey) {
+                (Some(a), Some(b)) => {
                     println!("Resolved {}", v);
                     let result = match op {
                         "+" => a + b,
@@ -50,20 +92,68 @@ pub fn part_one(input: &str) -> Option<usize> {
         for key in remove {
             pending.remove(&key);
         }
-        println!(
-            "After {} round(s): pending {:?} resolved {:?}",
-            round,
-            pending.len(),
-            resolved.len()
-        );
-        if resolved.contains_key("root") {
-            return Some(resolved.get("root").unwrap().clone());
+    }
+    // we're looking for the path from humn -> root and then we'll have to build up the answer... let's see how this goes
+    println!(
+        "After the resolving rounds: pending({:?})\n{:?}",
+        pending.len(),
+        pending
+    );
+    let mut mixed = BTreeMap::new();
+    // let's make one more pass through the pending, this time just replacing what we can.  From this point on, we'll have mixed expressions
+    for (k, v) in pending.iter() {
+        let mut tmp = String::from("");
+        let (a, op, b) = v.splitn(3, " ").next_tuple().unwrap();
+        if pending.contains_key(a) {
+            tmp.push_str(" (");
+            tmp.push_str(pending.get(a).unwrap());
+            tmp.push_str(")");
+        } else if resolved.contains_key(a) {
+            tmp.push_str(&resolved.get(a).unwrap().to_string());
+        } else {
+            tmp.push_str("stoopidhuman");
+        }
+
+        tmp.push_str(" ");
+        tmp.push_str(op);
+        tmp.push_str(" ");
+
+        if pending.contains_key(b) {
+            tmp.push_str(" (");
+            tmp.push_str(pending.get(b).unwrap());
+            tmp.push_str(")");
+        } else if resolved.contains_key(b) {
+            tmp.push_str(&resolved.get(b).unwrap().to_string());
+        } else {
+            tmp.push_str("stoopidhuman");
+        }
+        mixed.insert(k.clone(), tmp);
+    }
+
+    // dbg!(&mixed);
+
+    let root = mixed.get("root").unwrap();
+
+    // now let's replace until we don't replace any more
+    let re = Regex::new(r".*?(\w{4})+.*?").unwrap();
+
+    let mut subs = 0;
+    dbg!(&mixed);
+
+    loop {
+        let caps: Vec<regex::Captures> = re.captures_iter(root).collect();
+        println!("captured: {}", caps.len());
+        for c in caps.iter() {
+            let key = c.get(1).unwrap().as_str();
+            println!("{:?}", key);
+            let rep = match mixed.get(key) {
+                Some(val) => val,
+                None => key,
+            };
+            println!("cap[{}] = shoudl be replaced with {:?}", key, rep);
         }
     }
-    None
-}
 
-pub fn part_two(input: &str) -> Option<u32> {
     None
 }
 
@@ -86,6 +176,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 21);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(301));
     }
 }
