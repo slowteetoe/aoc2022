766e8f3f5cca241cd926a946181ef8b2d6a054aa
Steven Lotito <steven.lotito@gmail.com>
2022-12-29 09:02:33 -0800

    day 12 still not working, tried a_star - tiny bug somewhere =/

--- src/bin/12.rs
+++ src/bin/12.rs
@@ -1,15 +1,15 @@
-use std::collections::{BTreeMap, BTreeSet};
+use std::collections::BTreeMap;
 
 use advent_of_code::helpers;
-use priority_queue::PriorityQueue;
+use priq::PriorityQueue;
 
 // let's try something different, going to make a directed graph as we parse the grid
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
 pub struct Node {
     coord: (usize, usize),
-    elevation: char, // mostly for debugging, not like I ever need that =/
+    elevation: char,
     exits: Vec<(usize, usize)>,
-    cost: usize, // since we have to move to a* or dijkstra, we'll just use manhattan distance
+    cost: usize, // since we have to move to a*, we'll use manhattan distance
 }
 
 impl Node {
@@ -23,96 +23,65 @@ impl Node {
     }
 }
 
-// impl std::fmt::Debug for Node {
-//     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-//         f.debug_struct("Node")
-//             .field("coord", &self.coord)
-//             .field("elevation", &self.elevation)
-//             .field("cost", &self.cost)
-//             .field(
-//                 "exits",
-//                 &self
-//                     .exits
-//                     .iter()
-//                     .map(|n| {
-//                         let cell = Borrow::<RefCell<Node>>::borrow(n).borrow().coord;
-//                         format!("{:?}", cell)
-//                     })
-//                     .collect_vec()
-//                     .join(","),
-//             )
-//             .finish()
-//     }
-// }
-
-// FIXME implement a*
-// pub fn reconstruct_path(
-//     came_from: BTreeMap<(usize, usize), (usize, usize)>,
-//     current: (&(usize,usize), &usize),
-// ) -> Vec<(usize, usize)> {
-//     let mut total_path = vec![*current.0];
-//     for cur in came_from.keys() {
-//         total_path.push((cur.0, cur.1));
-//     }
-//     total_path
-// }
-
-// pub fn a_star(start: &Rc<RefCell<Node>>, goal: &Rc<RefCell<Node>>, h) -> Option<Vec<(usize, usize)>>{
-//     let mut open_set = PriorityQueue::new();
-//     let s = Borrow::<RefCell<Node>>::borrow(start).borrow();
-//     open_set.push(s.coord, s.cost);
-//     let came_from = BTreeMap::<(usize,usize), Rc<RefCell<Node>>>::new();
-//     let mut g_score = BTreeMap::new();
-//     g_score.insert(s.coord.clone(), 0);
-//     let mut f_score = BTreeMap::new();
-//     f_score.insert(s.coord, s.cost);
-
-//     while !open_set.is_empty() {
-//         let current = open_set.peek().unwrap();
-//         if *current.0 == Borrow::<RefCell<Node>>::borrow(goal).borrow().coord {
-//             return Some(reconstruct_path(came_from, current));
-//         }
-//         open_set.remove(current.0);
-//         for neighbor in
-//     }
-//     None
-// }
-
-pub fn walk_it(
-    curr: &Node,
-    path: &mut BTreeSet<(usize, usize)>,
-    map: &BTreeMap<(usize, usize), Node>,
-) -> Option<usize> {
-    let this_node = curr;
-    if this_node.elevation == 'E' {
-        println!("At target, found our path, {:?}", &path);
-        return Some(path.len());
+pub fn reconstruct_path(
+    came_from: BTreeMap<(usize, usize), (usize, usize)>,
+    current: ((usize, usize), usize),
+) -> Vec<(usize, usize)> {
+    println!("GOOOOAAAAL!!!");
+    let mut total_path = vec![];
+    let mut curr = current.0;
+    loop {
+        total_path.push(curr);
+        if curr == (0, 0) {
+            break;
+        }
+        curr = *came_from.get(&curr).unwrap();
     }
-    let pos = this_node.coord;
-    println!("Currently at {:?}, path so far: {:?}", &pos, &path);
-    for opt in this_node.exits.iter() {
-        println!("looking at {:?}", opt);
-        if !path.contains(opt) {
-            println!("Taking path option: {:?}", opt);
-            path.insert(pos.clone());
-            let result = walk_it(map.get(opt).unwrap(), path, map);
-            match result {
-                Some(n) => {
-                    if n < 33 {
-                        return Some(n);
-                    }
+    total_path.reverse();
+    total_path
+}
+
+pub fn a_star(
+    start: (usize, usize),
+    goal: (usize, usize),
+    map: &BTreeMap<(usize, usize), Node>,
+) -> Option<Vec<(usize, usize)>> {
+    let mut open_set = PriorityQueue::new();
+    let mut came_from = BTreeMap::<(usize, usize), (usize, usize)>::new();
+    let mut g_score = BTreeMap::new();
+
+    let start_node = map.get(&start).unwrap();
+
+    open_set.put(start_node.coord, start_node.cost);
+    g_score.insert(start_node.coord.clone(), 0);
+
+    loop {
+        if open_set.peek().is_none() {
+            break;
+        }
+        let current = open_set.pop().unwrap(); // this removes from the PQ
+        if current.0 == goal {
+            return Some(reconstruct_path(came_from, current));
+        }
+
+        for neighbor in &map.get(&current.0).unwrap().exits {
+            let tentative_g_score = g_score.get(&current.0).unwrap() + 1; // this is assuming d(current,neighbor) is one because they're all one hop away
+            if tentative_g_score < *g_score.entry(*neighbor).or_insert(usize::MAX) {
+                // this path to neighbor is better than any previous one, record it
+                came_from.insert(*neighbor, current.0);
+                g_score.insert(*neighbor, tentative_g_score);
+                let neighbor_cost = &map.get(neighbor).unwrap().cost;
+                let f = tentative_g_score + neighbor_cost;
+                if open_set.iter().find(|k| k.0 == *neighbor).is_none() {
+                    open_set.put(*neighbor, f);
                 }
-                _ => {}
             }
-            println!("have to backtrack");
-            path.remove(&pos.clone());
         }
     }
     None
 }
 
-pub fn parse(input: &str) -> BTreeMap<(usize, usize), Node> {
-    // if we hold onto the starting node, every node that we need to traverse will be attached, since there's a path to the exit
+pub fn parse(input: &str) -> (BTreeMap<(usize, usize), Node>, (usize, usize)) {
     let mut h = BTreeMap::new();
     // going to make two passes through the data, just to make things easier - first will construct all the nodes, second will connect them
     let mut max_x = 0;
@@ -120,21 +89,20 @@ pub fn parse(input: &str) -> BTreeMap<(usize, usize), Node> {
     let mut goal: Option<(usize, usize)> = None;
     input.lines().enumerate().for_each(|(y, line)| {
         line.chars().enumerate().for_each(|(x, ch)| {
-            let ch = match ch {
-                'S' => 'a',
-                // 'E' => 'z',
-                _ => ch,
-            };
             if ch == 'E' {
                 goal = Some((x, y));
             }
+            let ch = match ch {
+                'S' => 'a',
+                'E' => 'z',
+                _ => ch,
+            };
             h.insert((x, y), Node::new((x, y), ch));
             max_x = x;
         });
         max_y = y;
     });
     let goal = goal.unwrap();
-    println!("dim: ({:?},{:?}) with goal at: {:?}", max_x, max_x, goal);
 
     // now that we know where the goal is, we can compute cost (manhattan distance) from each node, as well as track the connected nodes
     input.lines().enumerate().for_each(|(y, line)| {
@@ -142,7 +110,6 @@ pub fn parse(input: &str) -> BTreeMap<(usize, usize), Node> {
             let mut this_node = h.remove(&(x, y)).unwrap();
             // update the cost
             this_node.cost = helpers::manhattan_usize((x, y), (goal.0, goal.1));
-            // check the cardinals
             if y > 0 {
                 let n = h.get(&(x, y - 1)).unwrap();
                 if n.elevation as u8 <= this_node.elevation as u8 + 1 {
@@ -170,17 +137,23 @@ pub fn parse(input: &str) -> BTreeMap<(usize, usize), Node> {
             h.insert((x, y), this_node);
         });
     });
-    println!("{:#?}", h);
-    h
+    (h, goal)
 }
 
 pub fn part_one(input: &str) -> Option<usize> {
-    let map = parse(input);
-    println!("{:?}", &map);
-    let root = map.get(&(0, 0)).unwrap();
-    let mut path = BTreeSet::<(usize, usize)>::new();
-    let answer = walk_it(root, &mut path, &map);
-    Some(answer.unwrap())
+    let (map, goal) = parse(input);
+
+    let solution = a_star((0, 0), goal, &map);
+    match solution {
+        Some(p) => {
+            // println!("path: {:?}", p);
+            Some(p.len() - 1)
+        }
+        None => {
+            println!("sad panda");
+            None
+        }
+    }
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
